1a08f239310fde8158255feb400239f8d5cb1758
Jarogna <>
2021-05-01 16:21:17 +0000

    Added Class Point. Taken from Dr. T's struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
-#include <iostream>
+//Convert Dr. T's struct to class and add Dr. T's To Do list.
+//Teacher: Dr. Tyson McMillian
+//Student: Ronald Angora
+//Date: 05/01/2021
 
-int main() {
-  std::cout << "Hello World!\n";
+#include <iostream>
+using namespace std;
+
+class Point
+{
+  private:
+    int x;
+    int y;
+  
+  public:
+    void setX(int);
+    void setY(int);
+    int getX() const;
+    int getY() const;
+};
+
+
+
+int main() 
+{
+  
 }
